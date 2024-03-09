595d40896b295d29c14167ad917799ccafcd8497
Rodrigo_vasquez <rvasquez26@gmail.com>
2024-03-09 14:31:41 -0300

    agrega registro dentro de app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import Registro from './components/Registro'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
- 
   return (
     <>
-     
+      <Registro/>
     </>
   )
 }
 
 export default App
+
