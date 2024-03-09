cf1a9cfa8655d15438fc7d6e986719a095e2b17d
Rodrigo_vasquez <rvasquez26@gmail.com>
2024-03-09 14:30:26 -0300

    crea registro de formulario

---
+++ src/components/Registro.jsx
@@ -0,0 +1,18 @@
+import Formulario from './Formulario'
+
+const Registro = () => {
+  return (
+    <>
+      <div className="container d-flex  justify-content-center align-items-center vh-100">
+        <div className="text-center p-4 bg-light border rounded-3">
+          <Formulario />
+        </div>
+      </div>
+
+
+    </>
+
+  )
+}
+
+export default Registro
